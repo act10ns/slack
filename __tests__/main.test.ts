e8682ff06f53c50b7f58c3ad04c242bc24cb89af
Nick Satterly <nfsatterly@gmail.com>
2020-05-10 13:55:07 +0200

    Code format tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -18,18 +18,42 @@ github.context.ref = dump.ref
 github.context.payload = dump.event
 
 test('slack message payload', async () => {
-  const mockAxios = new MockAdapter(axios, { delayResponse: 200 });
+  const mockAxios = new MockAdapter(axios, {delayResponse: 200})
 
-  mockAxios.onPost().reply((config) => {
-    console.log(config.data)
-    return [200, {status: 'ok'}]
-  }).onAny().reply(500)
+  mockAxios
+    .onPost()
+    .reply(config => {
+      console.log(config.data)
+      return [200, {status: 'ok'}]
+    })
+    .onAny()
+    .reply(500)
 
   const res = await send(url, jobName, jobStatus, jobSteps, channel)
-  await expect(res).toStrictEqual({text: {status: 'ok'}}) 
+  await expect(res).toStrictEqual({text: {status: 'ok'}})
 
-  expect(mockAxios.history.post[0].data)
-    .toBe(JSON.stringify({"username":"GitHub Action","icon_url":"https://octodex.github.com/images/original.png","channel":"@override","attachments":[{"fallback":"[GitHub]: [act10ns/slacky] build-test push Success","author_name":"satterly","author_link":"https://github.com/satterly","author_icon":"https://avatars0.githubusercontent.com/u/615057?v=4","mrkdwn_in":["text"],"text":"*<https://github.com/act10ns/slacky/commit/68d48876e0794fba714cb331a1624af6b20942d8/checks|Workflow _build-test_ job _Build and Test_ triggered by _push_ is _Success_> for <https://github.com/act10ns/slacky/compare/db9fe60430a6...68d48876e079|`master`>*\n<https://github.com/act10ns/slacky/commit/68d48876e0794fba714cb331a1624af6b20942d8|`68d48876`> - wip","fields":[],"footer":"<https://github.com/act10ns/slacky|act10ns/slacky>","footer_icon":"https://github.githubassets.com/favicon.ico","ts":1589052147}]}))
+  expect(mockAxios.history.post[0].data).toBe(
+    JSON.stringify({
+      username: 'GitHub Action',
+      icon_url: 'https://octodex.github.com/images/original.png',
+      channel: '@override',
+      attachments: [
+        {
+          fallback: '[GitHub]: [act10ns/slacky] build-test push Success',
+          author_name: 'satterly',
+          author_link: 'https://github.com/satterly',
+          author_icon: 'https://avatars0.githubusercontent.com/u/615057?v=4',
+          mrkdwn_in: ['text'],
+          text:
+            '*<https://github.com/act10ns/slacky/commit/68d48876e0794fba714cb331a1624af6b20942d8/checks|Workflow _build-test_ job _Build and Test_ triggered by _push_ is _Success_> for <https://github.com/act10ns/slacky/compare/db9fe60430a6...68d48876e079|`master`>*\n<https://github.com/act10ns/slacky/commit/68d48876e0794fba714cb331a1624af6b20942d8|`68d48876`> - wip',
+          fields: [],
+          footer: '<https://github.com/act10ns/slacky|act10ns/slacky>',
+          footer_icon: 'https://github.githubassets.com/favicon.ico',
+          ts: 1589052147
+        }
+      ]
+    })
+  )
 
   mockAxios.resetHistory()
   mockAxios.reset()
