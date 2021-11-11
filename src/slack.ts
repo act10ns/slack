3289813e80ae0a623e9c7457ca211856c28716fb
Nick Satterly <nfsatterly@gmail.com>
2021-11-11 09:43:15 +0100

    test2

--- src/slack.ts
+++ src/slack.ts
@@ -247,6 +247,17 @@ export async function send(
         title_link: opts?.title_link,
         text,
         fields,
+        "blocks": [
+          {
+            "type": "section",
+            "fields": [
+              {
+                "type": "plain_text",
+                "text": "*this is plain text*"
+              }
+            ]
+          }
+        ],
         fallback,
         footer,
         footer_icon: 'https://github.githubassets.com/favicon.ico',
